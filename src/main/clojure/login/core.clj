7d43cba18d7b59f8a7cde16d4c88e92e489b102a
coa5 <coa5@lipidappdev0.muh.musc.edu>
2011-11-04 15:45:15 -0400

    Fixed issues with auto login through shiboleth

--- src/main/clojure/login/core.clj
+++ src/main/clojure/login/core.clj
@@ -8,7 +8,7 @@
   (:gen-class
    :implements [com.liferay.portal.security.auth.AutoLogin]))
 
-(def company-id 0)
+(def company-id 10132)
 
 (defn -init
   []
@@ -17,7 +17,9 @@
 (defn get-credentials
   [email]
   (let [user (UserLocalServiceUtil/getUserByEmailAddress company-id email)
-        credentials (make-array (.getClass String )  3)]
+        credentials (make-array String 3)]
+    (println (.getUserId user))
+    (println (.getPassword user))
     (do
       (aset credentials 0 (str (.getUserId user)))
       (aset credentials 1 (str (.getPassword user)))
@@ -29,18 +31,18 @@
   (println (enumeration-seq (.getHeaderNames request)))
   (println (enumeration-seq (.getAttributeNames request)))
   (println (.getRemoteUser request))
-  (println (.getHeader request "Shib-EduPerson-Principal-Name"))
+  (println (.getHeader request "AJP_Shib-EduPerson-Principal-Name"))
   (println "*****************************")
   (try
     (println "here")
-    (let [user-id (.getHeader request "Shib-EduPerson-Principal-Name")
-          mail (.getHeader request "Shib-InetOrgPersonMail")
-          display-name (.getHeader request "Shib-Person-commonName")
+    (let [user-id (.getHeader request "AJP_Shib-EduPerson-Principal-Name")
+          mail (.getHeader request "AJP_Shib-InetOrgPersonMail")
+          display-name (.getHeader request "AJP_Shib-Person-commonName")
           original-request (PortalUtil/getOriginalServletRequest request) ]
       (println (enumeration-seq (.getHeaderNames original-request)))
       (println (enumeration-seq (.getAttributeNames original-request)))
       (println (.getRemoteUser original-request))
-      (println (.getHeader original-request "Shib-EduPerson-Principal-Name"))
+      (println (.getHeader original-request "AJP_Shib-EduPerson-Principal-Name"))
       (println "*****************************")
       (if mail
         (get-credentials mail)))
